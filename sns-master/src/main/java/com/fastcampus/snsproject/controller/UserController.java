0461c7ca5eec184004f7eb47cabff91c9f9b8562
jaehoonia <jaehoonia@naver.com>
2023-02-06 12:15:29 +0900

    Changes

---
+++ sns-master/src/main/java/com/fastcampus/snsproject/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.fastcampus.snsproject.controller;
+
+import com.fastcampus.snsproject.controller.request.UserJoinRequest;
+import com.fastcampus.snsproject.controller.request.UserLoginRequest;
+import com.fastcampus.snsproject.controller.response.AlarmResponse;
+import com.fastcampus.snsproject.controller.response.Response;
+import com.fastcampus.snsproject.controller.response.UserJoinResponse;
+import com.fastcampus.snsproject.controller.response.UserLoginResponse;
+import com.fastcampus.snsproject.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    // TODO : implement
+    @PostMapping("/join")
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
+         return Response.success(UserJoinResponse.fromUser(userService.join(request.getUserName(), request.getPassword())));
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
+        String token = userService.login(request.getUserName(), request.getPassword());
+        return Response.success(new UserLoginResponse(token));
+    }
+
+    @GetMapping("/alarm")
+    public Response<Page<AlarmResponse>> alarm(Pageable pageable, Authentication authentication) {
+        return Response.success(userService.alarmList(authentication.getName(), pageable).map(AlarmResponse::fromAlarm));
+    }
+}
